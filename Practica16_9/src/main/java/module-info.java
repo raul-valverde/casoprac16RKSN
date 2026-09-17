c9edd1072050646c3ce22a5cd717624b83434d8a
Steven Alexander flores Obregon <safloreso@uamv.edu.ni>
2026-09-16 23:12:49 -0600

    Update module-info.java
    fix: agregar permisos de export y opens en module-info para JavaFX

--- Practica16_9/src/main/java/module-info.java
+++ Practica16_9/src/main/java/module-info.java
@@ -3,6 +3,10 @@ module ni.edu.uam.practica16_9 {
     requires javafx.fxml;
 
 
-    opens ni.edu.uam.practica16_9 to javafx.fxml;
-    exports ni.edu.uam.practica16_9;
+    exports ni.edu.uam.practica16_9.application;
+    opens ni.edu.uam.practica16_9.application to javafx.fxml;
+
+
+    exports ni.edu.uam.practica16_9.controller;
+    opens ni.edu.uam.practica16_9.controller to javafx.fxml;
 }
