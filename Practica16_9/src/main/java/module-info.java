83b6408bdd5b78edaa3f20b4978e2dd5fddc8792
nora3071 <nmobregon@uamv.edu.ni>
2026-09-17 17:36:24 -0600

    fix|module-info
    Para exportar controladores

--- Practica16_9/src/main/java/module-info.java
+++ Practica16_9/src/main/java/module-info.java
@@ -5,4 +5,12 @@ module ni.edu.uam.practica16_9 {
 
     opens ni.edu.uam.practica16_9 to javafx.fxml;
     exports ni.edu.uam.practica16_9;
+
+
+    exports ni.edu.uam.practica16_9.controller;
+    opens ni.edu.uam.practica16_9.controller to javafx.fxml;
+
+
+    exports ni.edu.uam.practica16_9.model;
+    opens ni.edu.uam.practica16_9.model to javafx.fxml;
 }
