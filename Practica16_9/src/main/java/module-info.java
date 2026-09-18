1676ee4c6a33ee7d0853d234ddb6e4fb90569b41
Kellys Bellanger <kbbellanger@uamv.edu.ni>
2026-09-17 23:47:46 -0600

    refactor module info

--- Practica16_9/src/main/java/module-info.java
+++ Practica16_9/src/main/java/module-info.java
@@ -1,12 +1,15 @@
 module ni.edu.uam.practica16_9 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
 
-    // Permite que JavaFX acceda a tus vistas, controladores y clase principal
-    opens ni.edu.uam.practica16_9.appplication to javafx.graphics, javafx.fxml;
+    opens ni.edu.uam.practica16_9.application to javafx.graphics, javafx.fxml;
     opens ni.edu.uam.practica16_9.controller to javafx.fxml;
-    opens ni.edu.uam.practica16_9.model to javafx.fxml;
+    opens ni.edu.uam.practica16_9.model to javafx.base, javafx.fxml;
 
-    exports ni.edu.uam.practica16_9.appplication;
+    exports ni.edu.uam.practica16_9;
+    exports ni.edu.uam.practica16_9.application;
     exports ni.edu.uam.practica16_9.controller;
+    exports ni.edu.uam.practica16_9.model;
+    exports ni.edu.uam.practica16_9.repository;
 }
