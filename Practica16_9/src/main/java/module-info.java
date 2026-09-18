630f38b12d72206d19bc7dafa9f96451ef353d70
Raúl Valverde <raulvruiz@uamv.edu.ni>
2026-09-17 22:43:15 -0600

    feat: actualizo el module info

--- Practica16_9/src/main/java/module-info.java
+++ Practica16_9/src/main/java/module-info.java
@@ -3,7 +3,12 @@ module ni.edu.uam.practica16_9 {
     requires javafx.fxml;
     requires static lombok;
 
+    opens ni.edu.uam.practica16_9.controller to javafx.fxml;
+    opens ni.edu.uam.practica16_9.appplication to javafx.fxml;
+    opens ni.edu.uam.practica16_9.model to javafx.base, javafx.fxml;
 
-    opens ni.edu.uam.practica16_9 to javafx.fxml;
     exports ni.edu.uam.practica16_9;
+    exports ni.edu.uam.practica16_9.appplication;
+    exports ni.edu.uam.practica16_9.controller;
+    exports ni.edu.uam.practica16_9.model;
 }
