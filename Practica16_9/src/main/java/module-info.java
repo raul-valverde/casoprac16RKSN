fc4cfd4dee8e170ad78d26db41ab4bbbc4aeee59
Steven Alexander flores Obregon <safloreso@uamv.edu.ni>
2026-09-17 23:48:23 -0600

    Refactor modulo.info

--- Practica16_9/src/main/java/module-info.java
+++ Practica16_9/src/main/java/module-info.java
@@ -1,12 +1,15 @@
 module ni.edu.uam.practica16_9 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
 
-
-    exports ni.edu.uam.practica16_9.application;
-    opens ni.edu.uam.practica16_9.application to javafx.fxml;
-
-
-    exports ni.edu.uam.practica16_9.controller;
+    opens ni.edu.uam.practica16_9.application to javafx.graphics, javafx.fxml;
     opens ni.edu.uam.practica16_9.controller to javafx.fxml;
+    opens ni.edu.uam.practica16_9.model to javafx.base, javafx.fxml;
+
+    exports ni.edu.uam.practica16_9;
+    exports ni.edu.uam.practica16_9.application;
+    exports ni.edu.uam.practica16_9.controller;
+    exports ni.edu.uam.practica16_9.model;
+    exports ni.edu.uam.practica16_9.repository;
 }
