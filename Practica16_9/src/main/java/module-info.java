61a130d4d9000ff4ae04f8f20707d6fceda65b1a
nora3071 <nmobregon@uamv.edu.ni>
2026-09-17 23:47:32 -0600

    refactor| moduleinfo

--- Practica16_9/src/main/java/module-info.java
+++ Practica16_9/src/main/java/module-info.java
@@ -1,16 +1,15 @@
 module ni.edu.uam.practica16_9 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
 
-
-    opens ni.edu.uam.practica16_9 to javafx.fxml;
-    exports ni.edu.uam.practica16_9;
-
-
-    exports ni.edu.uam.practica16_9.controller;
+    opens ni.edu.uam.practica16_9.application to javafx.graphics, javafx.fxml;
     opens ni.edu.uam.practica16_9.controller to javafx.fxml;
+    opens ni.edu.uam.practica16_9.model to javafx.base, javafx.fxml;
 
-
+    exports ni.edu.uam.practica16_9;
+    exports ni.edu.uam.practica16_9.application;
+    exports ni.edu.uam.practica16_9.controller;
     exports ni.edu.uam.practica16_9.model;
-    opens ni.edu.uam.practica16_9.model to javafx.fxml;
+    exports ni.edu.uam.practica16_9.repository;
 }
