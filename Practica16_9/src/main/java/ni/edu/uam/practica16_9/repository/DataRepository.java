8c7a5f24dfac83d9564b52cdff50759609ecbad7
Raúl Valverde <raulvruiz@uamv.edu.ni>
2026-09-17 23:22:15 -0600

    fix, DataRepository con el nameList

--- Practica16_9/src/main/java/ni/edu/uam/practica16_9/repository/DataRepository.java
+++ Practica16_9/src/main/java/ni/edu/uam/practica16_9/repository/DataRepository.java
@@ -1,13 +1,13 @@
 package ni.edu.uam.practica16_9.repository;
 
-import ni.edu.uam.practica16_9.model.Cliente;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import ni.edu.uam.practica16_9.model.Cliente;
 
 public class DataRepository {
-    private static final ObservableList<Cliente> clientes = FXCollections.observableArrayList();
+    private static final ObservableList<Cliente> listaClientes = FXCollections.observableArrayList();
 
     public static ObservableList<Cliente> getClientes() {
-        return clientes;
+        return listaClientes;
     }
 }
