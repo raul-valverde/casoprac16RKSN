46b56f0645d81bc699b9ca7e43250f0da43677af
Kellys Bellanger <kbbellanger@uamv.edu.ni>
2026-09-17 22:11:38 -0600

    feat: configurar el main app para iniciar la aplicacion

--- Practica16_9/src/main/java/ni/edu/uam/practica16_9/appplication/MainApp.java
+++ Practica16_9/src/main/java/ni/edu/uam/practica16_9/appplication/MainApp.java
@@ -1,4 +1,27 @@
 package ni.edu.uam.practica16_9.appplication;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/ni/edu/uam/practica16_9/view/login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setTitle("Sistema de Gestión - Inicio de Sesión");
+        stage.setScene(scene);
+        stage.setResizable(false); //evita que deformemos la ventana
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
